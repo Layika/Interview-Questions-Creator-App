f9ab6ac459315da240ab0b3405bf638ffcbb9c17
Layika <zulayikanalukenge@gmail.com>
2026-06-03 19:20:14 +0300

    got rid of groq embeddings because they dont exist

--- src/helper.py
+++ src/helper.py
@@ -4,7 +4,7 @@ from langchain_core.documents import Document
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains.summarize import load_summarize_chain
-from langchain_groq import GroqEmbeddings
+from langchain_groq import GroqInterfaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_classic.chains import RetrievalQA
 from src.prompt import *
@@ -112,10 +112,7 @@ def llm_pipeline(file_path):
         chunk_size = 1000
     )
     """ 
-    embeddings = GroqEmbeddings(
-    model="bge-large-en-v1.5",
-    groq_api_key=os.getenv("GROQ_API_KEY")
-    )
+    embeddings = GroqInterfaceEmbeddings()
 
 
     vector_store = FAISS.from_documents(document_answer_gen, embeddings)
